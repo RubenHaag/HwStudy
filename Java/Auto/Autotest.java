fee82db8c8b72bc7005299f1f8e1ecd0846ed508
Unknown <rubenhaag1@gmail.com>
2016-12-14 18:25:09 +0100

    HA Fertig

--- Java/Auto/Autotest.java
+++ Java/Auto/Autotest.java
@@ -1,6 +1,6 @@
 class autotest{
-	Pkw
+	Pkw vw_Golf = new Pkw(5, 600, "Dreizylinder", 5, "VW", 5);
+	Lkw man_Lkw = new Lkw(50, true, "W16", 500, "MAN", 100);
 	public static void main(String[] args) {
-		
 	}
 }
