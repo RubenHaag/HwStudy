b8820570e7ac188c3a20318ae2011e66f68e5252
Ruben <rubenhaag1@gmail.com>
2017-02-09 17:26:04 +0100

    Fertig

--- Java/Versicherung/User.java
+++ Java/Versicherung/User.java
@@ -4,5 +4,8 @@ class User {
 		Policen[] liste = new Policen[100];
 		liste[0]= new Kfz("Tom","Haag",65.36,4);
 		liste[0].rechnungStellen();
+		liste[1]= new Hafti("Moritz", "Gerlach", 199.99, 5, 5000000, 4)
+		liste[1].rechnungStellen();
+		liste[1].auszahlen("Hat einen Windows Pc", 10000000000000)
 	}
 }
