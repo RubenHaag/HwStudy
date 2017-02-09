bc1311548f4a09ec3b4c5d3d2e08a2441c2fdd3b
Ruben <rubenhaag1@gmail.com>
2017-02-09 19:43:02 +0100

    Fertig

---
+++ Java/Versicherung/Hafti.java
@@ -0,0 +1,36 @@
+class Hafti extends Policen {
+	int alter;
+	int schadensdeckung;
+	int jährlicheErhöhung;
+	int risiko
+	Hafti(String name, String vorname, double minBeitrag, int alter, int schadensdeckung, int risiko){
+		this.name =name;
+		this.vorname =vorname;
+		this.minBeitrag =minBeitrag;
+		this.schadensdeckung = schadensdeckung;
+	}
+
+	
+	double multi;
+
+	public String rechnungStellen(){
+		switch(risiko){
+			case 4: multi=1.6; break;
+			case 3: multi=1.4; break;
+			case 2: multi=1.2; break;
+			case 1: multi=1; break;
+		}
+		return ("Die Rechnung von iher Lebensversicherung ist Da.\nSie müssen "+minBeitrag+"€ zahlen.\nDer zu zahlende beitrag ensteht durch ihren Mindesbeiteig ( "+minBeitrag+"€ ), der mit ihrer Risikostufe multipliziert wird");
+	}
+	public String auszahlen(){
+		return(" geht nicht")
+	}
+	public String auszahlen(String beschreibung, int schaden){
+		if(schaden < schadensdeckung){
+			return ("Da sie" +name+ "bei Folgendem Unglück \n" + beschreibung + "\n Den folgenden Schaden verusacht haben:\n " + schaden +"\n bekommen sie diesen abzüglich einer Selbstbeteiligung von:\n" + risiko * 100 +"erstattet");
+		}
+		else{
+						return ("Da sie" +name+ "bei Folgendem Unglück \n" + beschreibung + "\n Den folgenden Schaden verusacht haben:\n " + schaden +"\n , und dieser Schaden ihre maximale Schadensdeckung von " +schadensdeckung+"€ übersteigt bekommen sie jene abzüglich einer Selbstbeteiligung von:\n" + risiko * 100 +"erstattet");
+		}
+	}
+}
---
+++ Java/Versicherung/Leben.java
@@ -0,0 +1,31 @@
+class Leben extends Policen {
+	int alter;
+	int schadensdeckung;
+	int jährlicheErhöhung;
+	int risiko
+	Leben(String name, String vorname, double minBeitrag, int alter, int schadensdeckung, int risiko){
+		this.name =name;
+		this.vorname =vorname;
+		this.minBeitrag =minBeitrag;
+		this.schadensdeckung = schadensdeckung;
+	}
+
+	
+	double multi;
+
+	public String rechnungStellen(){
+		switch(risiko){
+			case 4: multi=1.6; break;
+			case 3: multi=1.4; break;
+			case 2: multi=1.2; break;
+			case 1: multi=1; break;
+		}
+		return ("Die Rechnung von iher Lebensversicherung ist Da.\nSie müssen "+minBeitrag+"€ zahlen.\nDer zu zahlende beitrag ensteht durch ihren Mindesbeiteig ( "+minBeitrag+"€ ), der mit ihrer Risikostufe multipliziert wird");
+	}
+	public String auszahlen(){
+		return(" geht nicht")
+	}
+	public String auszahlen(String beschreibung){
+		return ("Da ihr Verwandter" +name+ "bei Folgendem Unfall verunglück ist \n" + beschreibung + "\n erhalten sie einen Schadensersatz von " + schadensdeckung);
+	}
+}
