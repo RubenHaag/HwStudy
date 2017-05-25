bcc7a9f5da7b8295992698ffef7838d052e3b130
RubenHaag <rubenhaag1@gmail.com>
2017-05-25 15:45:20 +0200

    Up

---
+++ Java/Hashing/Hashing.java
@@ -0,0 +1,17 @@
+import java.lang.String;
+import java.lang.Math.*;
+class Hashing{
+
+	int hashfunktion(String s){
+		if (s.isEmpty()){
+			return 0;
+		}
+		int x = 0;
+		char [] c = s.toCharArray();
+		int l = s.length();
+		for(int i = 0; i < l; i++){
+			x += ((int) c[i]) * (int)Math.pow(31, l-1);
+		}
+		return x;
+	}
+}
---
+++ Java/Hashing/Verbrecherhash.java
@@ -0,0 +1,3 @@
+class Verbrecherhash extends {
+	
+}
