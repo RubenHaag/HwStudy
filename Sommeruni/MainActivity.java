5a8f80c73aeda2107b37825533823b72834c77ac
RubenHaag <rubenhaag1@gmail.com>
2017-08-29 14:40:47 +0200

    Update MainActivity.java

--- Sommeruni/MainActivity.java
+++ Sommeruni/MainActivity.java
@@ -5,28 +5,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+//
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Rechner{
+    char op;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void buttonFunk(View v){
-        //Zahl 1 wird eingelesen
+    /*public String feldAuslesen(){
         EditText zahl1 = (EditText) findViewById(R.id.editText);
-        String s1 = zahl1.getText().toString();
-        Integer nr = Integer.parseInt(s1);
+        return zahl1.getText().toString();
+    }*/
+/*    public void add(View v){
+        //Zahl 1 wird eingelesen
+            EditText zahl1 = (EditText) findViewById(R.id.editText);
+            String s1 = zahl1.getText().toString();
+            Integer nr = Integer.parseInt(s1);
 
-        //Zahl 2 wird eingelesen
-        EditText zahl2 = (EditText) findViewById(R.id.editText2);
-        s1 = zahl2.getText().toString();
-        Integer nr2 = Integer.parseInt(s1);
+            //Zahl 2 wird eingelesen
+            EditText zahl2 = (EditText) findViewById(R.id.editText2);
+            s1 = zahl2.getText().toString();
+            Integer nr2 = Integer.parseInt(s1);
 
-        Integer ergebnis = nr + nr2;
-        String lösung = String.valueOf(ergebnis);
-        TextView ausgabe = (TextView) findViewById(R.id.Ergebnis);
-        ausgabe.setText("" + nr + "+" + nr2 + "=" + lösung);
+            Integer ergebnis = nr + nr2;
+            TextView ausgabe = (TextView) findViewById(R.id.Ergebnis);
+            ausgabe.setText("Ergebnis:\n" + nr + " + " + nr2 + " = " + ergebnis);
     }
+    public void mult(View v){
+        try {
+            EditText zahl1 = (EditText) findViewById(R.id.editText);
+            String s1 = zahl1.getText().toString();
+            Integer nr = Integer.parseInt(s1);
+
+            //Zahl 2 wird eingelesen
+            EditText zahl2 = (EditText) findViewById(R.id.editText2);
+            s1 = zahl2.getText().toString();
+            Integer nr2 = Integer.parseInt(s1);
+
+            Integer ergebnis = nr * nr2;
+            TextView ausgabe = (TextView) findViewById(R.id.Ergebnis);
+            ausgabe.setText("Ergebnis:\n" + nr + " * " + nr2 + " = " + ergebnis);
+        }
+        catch (Exception e){
+            Toast.makeText(this, "Bitte nur ganze Zahlen eingeben\n" + e, Toast.LENGTH_LONG).show();
+        }
+    }*/
 }
